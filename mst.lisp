df35c1e776e2707c2e2b4c4cac098c97cd5532be
mirkobi89 <38865531+mirkobi89@users.noreply.github.com>
2020-12-25 18:58:41 +0100

    25-12-20
    graph-vertices works well

---
+++ mst.lisp
@@ -0,0 +1,58 @@
+(defparameter *vertices* (make-hash-table :test #'equal))
+
+(defparameter *graphs* (make-hash-table :test #'equal))
+
+(defparameter *arcs* (make-hash-table :test #'equal))
+
+(defparameter *visited* (make-hash-table :test #'equal))
+
+(defparameter *vertex-keys* (make-hash-table :test #'equal))
+
+(defparameter *previous* (make-hash-table :test #'equal))
+
+(defun print-graphs()
+  (maphash (lambda (k v)(format t "Key: ~S, value: ~S ~%" k v)) *graphs*))
+
+(defun is-graph (graph-id)
+  (gethash graph-id *graphs*))
+
+(defun new-graph (graph-id)
+  (or (gethash graph-id *graphs*)
+      (setf (gethash graph-id *graphs*) graph-id)))
+
+;;(defun delete-graph (graph-id)
+;;(remhash (list 'vertex graph-id vertex-id) *vertices*))
+
+(defun new-vertex (graph-id vertex-id)
+  (setf (gethash (list 'vertex graph-id vertex-id)
+		 *vertices*)
+  (list 'vertex graph-id vertex-id)))
+
+(defparameter v-list'())
+(defun loop-l (lista graph-id)
+  (cond ((null lista) nil)
+	((eql graph-id (nth 1 (first lista)))
+	 (push (first lista) v-list)
+	 (loop-l (rest lista) graph-id)
+	 )
+        ((loop-l (rest lista) graph-id)))
+  )
+
+(defun graph-vertices (graph-id)
+  (let (l (collect-hash-table *vertices*)))
+  (loop-l l graph-id)
+  v-list
+)
+
+
+
+(defun collect-hash-table (ht)
+  (let (kvs)
+  (maphash #'(lambda (k v) (push k kvs)) ht)
+  kvs))
+
+
+
+;(defun new-arc (graph-id vertex-id vertex-id &weight))
+
+;(defun graph-arcs (graph-id))
