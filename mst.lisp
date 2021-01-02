2de6d538da4e85e26c47a4eede3d02de2789b5fd
mirkobi89 <38865531+mirkobi89@users.noreply.github.com>
2021-01-02 10:23:49 +0100

    heap-modify-key e heap-insert funzionano

--- mst.lisp
+++ mst.lisp
@@ -10,6 +10,9 @@
 
 (defparameter *previous* (make-hash-table :test #'equal))
 
+(defparameter *heaps* (make-hash-table :test #'equal))
+
+
 (defun is-graph (graph-id)()
   (gethash graph-id *graphs*))
 
@@ -59,4 +62,157 @@
 
 (defun graph-vertex-neighbors (graph-id vertex-id)
   (funcall 'collect-hash-table *arcs* graph-id vertex-id))
+
+;;(defun graph-vertex-adjacent (graph-id vertex-id))
+
+(defun graph-print (graph-id)
+  (let ((x (funcall 'graph-vertices graph-id))
+	(y (funcall 'graph-arcs graph-id)))
+    (format t "Vertici: ~S ~%Archi: ~S ~%" x y)))
+
+;;MINHEAP
+
+(defparameter heap-rep '())
+
+(defparameter heap '())
+
+(defun new-heap (heap-id &optional (capacity 42))
+  (or (gethash heap-id *heaps*)
+      (setf (gethash heap-id *heaps*)
+	    (list 'heap heap-id 0 (make-array capacity)))))
+
+(defun heap-id (heap-rep)
+  (if (null heap-rep)
+      nil)
+  (second heap-rep))
+
+(defun heap-size (heap-rep)
+  (if (null heap-rep)
+      nil)
+  (third heap-rep))
+
+(defun heap-actual-heap (heap-rep)
+  (if (null heap-rep)
+      nil)
+  (fourth heap-rep))
+
+(defun heap-delete (heap-id)
+  (remhash heap-id *heaps*))
+
+(defun heap-empty (heap-id)
+  (let ((x  (funcall 'heap-size (gethash heap-id *heaps*))))
+    (= x 0)))
+
+(defun heap-not-empty (heap-id)
+  (null (heap-empty heap-id)))
+
+(defun heap-head (heap-id)
+  (aref (heap-actual-heap (gethash heap-id *heaps*)) 0))
+      
+(defun heap-extract (heap-id)
+  (if (< (heap-size heap-rep) 1)
+      nil)
+  (let ((head (heap-head heap-id)))
+    (delete head heap)
+	(heapify 0)
+    head))
+
+(defun swim (i)
+  (cond ((<= i 0) nil)
+	((< (first (aref heap (parent i))) (first (aref heap i))) nil)
+	(t (scambia heap i (parent i))
+	   (swim (parent i)))))
+
+(defun heap-modify-key (heap-id new-key old-key v)
+  (if (> new-key old-key)
+      nil)
+  (let ((i (position old-key heap :key #'first)))
+    (setf (aref heap i) (list new-key v))
+     (swim i)))
+
+(defun heap-insert (heap-id k v)
+  (if (= (heap-size heap-rep) (length heap))
+      nil
+  (let ((hs (heap-size heap-rep)))
+    (setf (third heap-rep) (1+ hs))
+    (setf (aref heap (1- (heap-size heap-rep))) (list k v))
+    (swim (1- hs)))))
+  
+(defun create-heap()
+  (setf heap-rep (new-heap 'h1 10))
+  (setf (third heap-rep) 9)
+  (setf heap (heap-actual-heap heap-rep))
+  (setf (aref heap 0) (list 1 'a))
+  (setf (aref heap 1) (list 14 'b))
+  (setf (aref heap 2) (list 3 'c))
+  (setf (aref heap 3) (list 4 'd))
+  (setf (aref heap 4) (list 7 'e))
+  (setf (aref heap 5) (list 8 'f))
+  (setf (aref heap 6) (list 9 'g))
+  (setf (aref heap 7) (list 10 'h))
+  (setf (aref heap 8) (list 2 'i))
+  ;(setf (aref heap 9) (list 16 'l))
+  )
+
+(defun let-function ()
+  (let ((x 2))
+    (format t "x nel 1° let: ~a~%" x)
+    (let ((y (+ x 10)))
+      (format t "x nel 2° let: ~a~%" x)
+      (list x y)
+      )))
+
+(defun let-function-2()
+  (let ((smallest 2))
+	   (if (> 1 0)
+	       (smallest 3)
+	       (smallest 4))))
+
+(defun parent (i)
+  (floor (- i 1) 2))
+
+(defun left(i)
+  (+ (* 2 i) 1))
+
+(defun right(i)
+  (+ (* 2 i) 2))
+
+(defun scambia (heap i smallest)
+  (let ((tmp (aref heap i)))
+    (setf (aref heap i) (aref heap smallest)
+	  (aref heap smallest) tmp)))
+
+(defun heapify (i)
+  (let ((r (right i))
+	(l (left i))
+	(smallest i))
+	(if (and (< l (heap-size heap-rep))
+	    (< (first(aref heap l))(first(aref heap i))))
+	    (setf smallest l))
+	(if (and (< r (heap-size heap-rep))
+	    (< (first(aref heap r)) (first(aref heap smallest))))
+	    (setf smallest r))
+	(if (/= smallest i)
+	    (scambia heap i smallest)
+	    (heapify smallest))))
+;; (defparameter r '())
+;; (defparameter l '())
+;; (defparameter smallest '())
+
+;; (defun heapify (heap i)
+;;   (setf r (right i))
+;;   (setf l (left i))
+;;   (setf smallest i)
+;; 	(if (and (< l (heap-size heap-rep))
+;; 	    (< (aref heap l)(aref heap i)))
+;; 	    (setf smallest l))
+;; 	(if (and (< r (heap-size heap-rep))
+;; 	    (< (aref heap r) (aref heap smallest)))
+;; 	    (setf smallest r))
+;; 	(if (/= smallest i)
+;; 	    (scambia heap i smallest)
+;; 	    (heapify heap smallest)))
+
+;(defun build-min-heap (heap-id)
+ ; (setf (third heap-rep) (length heap))
   
